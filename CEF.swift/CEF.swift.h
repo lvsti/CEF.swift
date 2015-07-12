64b7e3c511c040402166b934f8fdb1461d51e9f7
Lk Xf <elveestei@gmail.com>
2015-07-12 17:43:22 +0200

    Initial Commit

---
+++ CEF.swift/CEF.swift.h
@@ -0,0 +1,19 @@
+//
+//  CEF.swift.h
+//  CEF.swift
+//
+//  Created by Tamas Lustyik on 2015. 07. 12..
+//  Copyright © 2015. Tamas Lustyik. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CEF.swift.
+FOUNDATION_EXPORT double CEF_swiftVersionNumber;
+
+//! Project version string for CEF.swift.
+FOUNDATION_EXPORT const unsigned char CEF_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CEF_swift/PublicHeader.h>
+
+
