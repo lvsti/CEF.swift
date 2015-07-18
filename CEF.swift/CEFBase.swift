77a9d8dbebe1932acfca69a6ccc2b9c82e8847e0
Tamas Lustyik <elveestei@gmail.com>
2015-07-18 17:51:03 +0200

    Base class for handler types

--- CEF.swift/CEFBase.swift
+++ CEF.swift/CEFBase.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public protocol CEFObject {
-    var base: cef_base_t { get nonmutating set }
+    var base: cef_base_t { get set }
 }
 
 protocol CEFBase: class, CEFRefCounting {
@@ -80,3 +80,117 @@ public class CEFProxyBase<T : CEFObject>: CEFBase, CEFProxyRefCounting {
         return self.init(ptr: ptr)
     }
 }
+
+
+protocol CEFHandlerRefCounting: CEFRefCounting {
+    func refCountLock()
+    func refCountUnlock()
+    var _refCount: Int { get set }
+}
+
+extension CEFHandlerRefCounting where Self : CEFBase, Self : CEFObjectLookupAdapter {
+    func addRef() {
+        refCountLock()
+        defer { refCountUnlock() }
+        
+        ++_refCount
+        if _refCount == 1 {
+            self.registerSelf()
+        }
+    }
+    
+    func release() -> Bool {
+        refCountLock()
+        defer { refCountUnlock() }
+        
+        --_refCount
+        if _refCount == 0 {
+            self.deregisterSelf()
+            return true
+        }
+        
+        return false
+    }
+    
+    func hasOneRef() -> Bool {
+        refCountLock()
+        defer { refCountUnlock() }
+        
+        return _refCount == 1
+    }
+}
+
+
+protocol CEFObjectLookupAdapter {
+    func registerSelf()
+    func deregisterSelf()
+}
+
+protocol CEFObjectLookup {
+    typealias ObjectType
+    typealias SelfType
+    
+    static var _registry: [UnsafeMutablePointer<ObjectType>: SelfType] { get set }
+    static var _registryMutex: pthread_mutex_t { get set }
+
+    static func lookup(ptr: UnsafeMutablePointer<ObjectType>) -> SelfType?
+}
+
+extension CEFObjectLookupAdapter where Self : CEFBase, Self : CEFObjectLookup, Self == Self.SelfType {
+    func registerSelf() {
+        pthread_mutex_lock(&Self._registryMutex)
+        defer { pthread_mutex_unlock(&Self._registryMutex) }
+        
+        Self._registry[cefObjectPtr] = self
+    }
+    
+    func deregisterSelf() {
+        pthread_mutex_lock(&Self._registryMutex)
+        defer { pthread_mutex_unlock(&Self._registryMutex) }
+        
+        Self._registry.removeValueForKey(cefObjectPtr)
+    }
+
+    static func lookup(ptr: UnsafeMutablePointer<ObjectType>) -> SelfType? {
+        pthread_mutex_lock(&_registryMutex)
+        defer { pthread_mutex_unlock(&_registryMutex) }
+        
+        return _registry[ptr]
+    }
+}
+
+
+public class CEFHandlerBase<T : CEFObject>: CEFBase, CEFHandlerRefCounting, CEFObjectLookupAdapter {
+    typealias ObjectType = T
+    typealias ObjectPtrType = UnsafeMutablePointer<T>
+    
+    private let _cefPtr: ObjectPtrType
+    var cefObjectPtr: UnsafeMutablePointer<ObjectType> { get { return _cefPtr } set {} }
+    var cefObject: ObjectType { get { return _cefPtr.memory } set {} }
+
+    var _refMutex: pthread_mutex_t = pthread_mutex_t()
+    var _refCount: Int = 0
+    
+    func refCountLock() {
+        pthread_mutex_lock(&_refMutex)
+    }
+    
+    func refCountUnlock() {
+        pthread_mutex_unlock(&_refMutex)
+    }
+
+    init(ptr: ObjectPtrType) {
+        _cefPtr = ptr
+    }
+
+    deinit {
+        _cefPtr.dealloc(1)
+    }
+    
+    func registerSelf() {
+    }
+    
+    func deregisterSelf() {
+    }
+}
+
