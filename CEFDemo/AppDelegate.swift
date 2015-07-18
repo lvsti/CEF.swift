7648db8ca0c38423ee8e34a95d7efc56c643a18a
Tamas Lustyik <elveestei@gmail.com>
2015-07-18 23:30:14 +0200

    Created demo app

---
+++ CEFDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CEFDemo
+//
+//  Created by Tamas Lustyik on 2015. 07. 18..
+//  Copyright © 2015. Tamas Lustyik. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CEFDemo/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  CEF.swift
+//
+//  Created by Tamas Lustyik on 2015. 07. 18..
+//  Copyright © 2015. Tamas Lustyik. All rights reserved.
+//
+
+import Cocoa
+import CEFswift
+
