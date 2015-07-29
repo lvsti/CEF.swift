96356454dffa4c417d7592c9053d0b53fa8e6ddc
Tamas Lustyik <elveestei@gmail.com>
2015-07-29 21:27:06 +0200

    Still refactoring

--- CEF.swift/CEFClient.swift
+++ CEF.swift/CEFClient.swift
@@ -11,14 +11,11 @@ import Foundation
 extension cef_client_t: CEFObject {
 }
 
-class CEFClientMarshaller: CEFMarshaller<cef_client_t, CEFClient> {
-    override init(obj: CEFClient) {
-        super.init(obj: obj)
-    }
-}
+typealias CEFClientMarshaller = CEFMarshaller<CEFClient>
 
-public class CEFClient: CEFHandler {
-    
+public class CEFClient: CEFHandler, CEFMarshallable {
+    typealias StructType = cef_client_t
+
     public override init() {
         super.init()
     }
@@ -64,7 +61,7 @@ public class CEFClient: CEFHandler {
         return nil
     }
 */
-    func getLifeSpanHandler() -> CEFLifeSpanHandler? {
+    public func getLifeSpanHandler() -> CEFLifeSpanHandler? {
         return nil
     }
 /*
@@ -90,8 +87,37 @@ public class CEFClient: CEFHandler {
     func toCEF() -> UnsafeMutablePointer<cef_client_t> {
         return CEFClientMarshaller.pass(self)
     }
+    
+    func marshalCallbacks(inout cefStruct: cef_client_t) {
+//        cefStruct.get_context_menu_handler
+//        cefStruct.get_dialog_handler
+//        cefStruct.get_display_handler
+//        cefStruct.get_download_handler
+//        cefStruct.get_drag_handler
+//        cefStruct.get_find_handler
+//        cefStruct.get_focus_handler
+//        cefStruct.get_geolocation_handler
+//        cefStruct.get_jsdialog_handler
+//        cefStruct.get_keyboard_handler
+        cefStruct.get_life_span_handler = CEFClient_getLifeSpanHandler
+//        cefStruct.get_load_handler
+//        cefStruct.get_render_handler
+//        cefStruct.get_request_handler
+//        cefStruct.on_process_message_received
+    }
 }
 
+func CEFClient_getLifeSpanHandler(ptr: UnsafeMutablePointer<cef_client_t>) -> UnsafeMutablePointer<cef_life_span_handler_t> {
+    guard let obj = CEFClientMarshaller.get(ptr) else {
+        return nil
+    }
+    
+    if let handler = obj.getLifeSpanHandler() {
+        return handler.toCEF()
+    }
+    
+    return nil
+}
 
 
 
