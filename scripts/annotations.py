e9c681c3b2167395fa6c4b1df65cd2c238ce85c8
Tamas Lustyik <elveestei@gmail.com>
2015-08-03 22:05:39 +0200

    Enum generator

---
+++ scripts/annotations.py
@@ -0,0 +1,144 @@
+#!/usr/bin/python
+
+abbreviations = [
+    'cef', 'http', 'https', 'ssl', 'url', 'uri', 'json', 'html', 'xhtml', 'v8', 'dom',
+    'cdata', 'utf8', 'utf16le', 'utf16be', 'ascii', 'io', 'ui', 'db', '5xx', 'xhr',
+    'cmy', 'cmyk', 'kcmy', 'rgb', 'rgba', 'rgb16', 'hp', 'rfc', 'id', 'js', 'xml'
+]
+
+typename_overrides = {
+    'cef_v8_accesscontrol_t': 'CEFV8AccessControl',
+    'cef_v8_propertyattribute_t': 'CEFV8PropertyAttribute',
+    'cef_postdataelement_type_t': 'CEFPOSTDataElementType',
+    'cef_post_data_element_t': 'CEFPOSTDataElement',
+    'cef_post_data_t': 'CEFPOSTData',
+    'cef_urlrequest_flags_t': 'CEFURLRequestFlags',
+    'cef_urlrequest_status_t': 'CEFURLRequestStatus',
+    'cef_jsdialog_type_t': 'CEFJSDialogType',
+    'cef_errorcode_t': 'CEFErrorCode'
+}
+
+entryname_overrides = {
+    'RT_SUB_FRAME': 'Subframe',
+    'RT_STYLESHEET': 'StyleSheet',
+    'RT_SUB_RESOURCE': 'Subresource',
+    'COLOR_MODEL_CMY_K': 'CMYPlusK',
+    'COLOR_MODEL_COLORMODE_COLOR': 'ColorModeColor',
+    'COLOR_MODEL_COLORMODE_MONOCHROME': 'ColorModeMonochrome',
+    'COLOR_MODEL_PRINTOUTMODE_NORMAL': 'PrintoutModeNormal',
+    'COLOR_MODEL_PRINTOUTMODE_NORMAL_GRAY': 'PrintoutModeNormalGray',
+    'COLOR_MODEL_PROCESSCOLORMODEL_CMYK': 'ProcessColorModelCMYK',
+    'COLOR_MODEL_PROCESSCOLORMODEL_GREYSCALE': 'ProcessColorModelGrayscale',
+    'COLOR_MODEL_PROCESSCOLORMODEL_RGB': 'ProcessColorModelRGB',
+    'CT_IBEAM': 'IBeam',
+    'CT_EASTRESIZE': 'EastResize',
+    'CT_NORTHRESIZE': 'NorthResize',
+    'CT_NORTHEASTRESIZE': 'NEResize',
+    'CT_NORTHWESTRESIZE': 'NWResize',
+    'CT_SOUTHRESIZE': 'SouthResize',
+    'CT_SOUTHEASTRESIZE': 'SEResize',
+    'CT_SOUTHWESTRESIZE': 'SWResize',
+    'CT_WESTRESIZE': 'WestResize',
+    'CT_NORTHSOUTHRESIZE': 'NSResize',
+    'CT_EASTWESTRESIZE': 'EWResize',
+    'CT_NORTHEASTSOUTHWESTRESIZE': 'NESWResize',
+    'CT_NORTHWESTSOUTHEASTRESIZE': 'NWSEResize',
+    'CT_COLUMNRESIZE': 'ColumnResize',
+    'CT_ROWRESIZE': 'RowResize',
+    'CT_MIDDLEPANNING': 'MiddlePanning',
+    'CT_EASTPANNING': 'EastPanning',
+    'CT_NORTHPANNING': 'NorthPanning',
+    'CT_NORTHEASTPANNING': 'NEPanning',
+    'CT_NORTHWESTPANNING': 'NWPanning',
+    'CT_SOUTHPANNING': 'SouthPanning',
+    'CT_SOUTHEASTPANNING': 'SEPanning',
+    'CT_SOUTHWESTPANNING': 'SWPanning',
+    'CT_WESTPANNING': 'WestPanning',
+    'CT_VERTICALTEXT': 'VerticalText',
+    'CT_CONTEXTMENU': 'ContextMenu',
+    'CT_NODROP': 'NoDrop',
+    'CT_NOTALLOWED': 'NotAllowed',
+    'CT_ZOOMIN': 'ZoomIn',
+    'CT_ZOOMOUT': 'ZoomOut',
+    'V8_PROPERTY_ATTRIBUTE_READONLY': 'ReadOnly',
+    'V8_PROPERTY_ATTRIBUTE_DONTENUM': 'DontEnumerate',
+    'V8_PROPERTY_ATTRIBUTE_DONTDELETE': 'DontDelete',
+    'MENU_ID_RELOAD_NOCACHE': 'ReloadNoCache',
+    'MENU_ID_STOPLOAD': 'StopLoad',
+    'KEYEVENT_RAWKEYDOWN': 'RawKeyDown',
+    'KEYEVENT_KEYDOWN': 'KeyDown',
+    'KEYEVENT_KEYUP': 'KeyUp',
+    'DOM_EVENT_CATEGORY_XMLHTTPREQUEST_PROGRESS': 'XMLHTTPRequestProgress',
+    'ST_LOCALSTORAGE': 'LocalStorage',
+    'ST_SESSIONSTORAGE': 'SessionStorage'
+}
+
+entrytype_tocef_cast_overrides = {
+    'cef_errorcode_t': 'Int32',
+    'cef_duplex_mode_t': 'Int32'
+}
+
+entryvalue_overrides = {
+    'UINT_MAX' : 'UInt32.max',
+}
+
+entries_render_as_property = {
+    'MENU_ID_SPELLCHECK_SUGGESTION_LAST'
+}
+
+cef_enums = [
+    'cef_log_severity_t',
+    'cef_state_t',
+    'cef_return_value_t',
+    'cef_termination_status_t',
+    'cef_path_key_t',
+    'cef_storage_type_t',
+    'cef_errorcode_t',
+    'cef_window_open_disposition_t',
+    'cef_postdataelement_type_t',
+    'cef_resource_type_t',
+    'cef_urlrequest_status_t',
+    'cef_process_id_t',
+    'cef_thread_id_t',
+    'cef_value_type_t',
+    'cef_jsdialog_type_t',
+    'cef_menu_id_t',
+    'cef_mouse_button_type_t',
+    'cef_paint_element_type_t',
+    'cef_menu_item_type_t',
+    'cef_context_menu_media_type_t',
+    'cef_key_event_type_t',
+    'cef_focus_source_t',
+    'cef_navigation_type_t',
+    'cef_xml_encoding_type_t',
+    'cef_xml_node_type_t',
+    'cef_dom_document_type_t',
+    'cef_dom_event_phase_t',
+    'cef_dom_node_type_t',
+    'cef_geoposition_error_code_t',
+    'cef_color_model_t',
+    'cef_duplex_mode_t',
+    'cef_cursor_type_t',
+    'cef_json_parser_error_t',
+]
+
+cef_option_sets = [
+    'cef_drag_operations_mask_t',
+    'cef_v8_accesscontrol_t',
+    'cef_v8_propertyattribute_t',
+    'cef_urlrequest_flags_t',
+    'cef_event_flags_t',
+    'cef_context_menu_type_flags_t',
+    'cef_context_menu_media_state_flags_t',
+    'cef_context_menu_edit_state_flags_t',
+    'cef_dom_event_category_t',
+    'cef_uri_unescape_rule_t',
+    'cef_json_parser_options_t',
+    'cef_json_writer_options_t'
+]
+
+cef_hybrid_enums = [
+    'cef_transition_type_t',
+    'cef_file_dialog_mode_t',
+]
+
---
+++ scripts/gen_enum.py
@@ -0,0 +1,219 @@
+#!/usr/bin/python
+
+import sys
+import os
+import re
+import string
+from annotations import *
+
+
+
+def indent_multiline_string(mls, level):
+    retval = ""
+    for line in mls.splitlines(True):
+        retval += "\t".expandtabs(4*level) + line.lstrip()
+    return retval
+
+def cef_capi_enum_entry_to_swift(capi_entry, prefix):
+    if capi_entry in entryname_overrides:
+        return entryname_overrides[capi_entry]
+    
+    retval = ""
+    for word in capi_entry[len(prefix):].split("_"):
+        if word.lower() in abbreviations:
+            retval += word.upper()
+        else:
+            retval += word.capitalize()
+    
+    return retval
+
+def cef_capi_enum_value_to_swift(capi_value):
+    if capi_value in entryvalue_overrides:
+        return entryvalue_overrides[capi_value]
+    
+    return capi_value
+
+def cef_capi_type_to_swift(capi_type):
+    if capi_type in typename_overrides:
+        return typename_overrides[capi_type]
+    
+    retval = "CEF"
+    for word in capi_type.split("_")[1:-1]:
+        if word.lower() in abbreviations:
+            retval += word.upper()
+        else:
+            retval += word.capitalize()
+    
+    return retval
+
+def common_prefix(m):
+    if not m: return ''
+    s1 = min(m)
+    s2 = max(m)
+    for i, c in enumerate(s1):
+        if c != s2[i]:
+            return s1[:i]
+    return s1    
+
+
+enum_type_pattern = re.compile("(\/\/\/\s*\n(?:\s*\/\/(?!\/)[^\n]*\n)+\s*\/\/\/\s*)\s*typedef\s+enum\s*{\s*([^}]*?)\s*}\s*([^;]+);", re.S)
+enum_value_pattern = re.compile("(\/\/\/\s*\n(?:\s*\/\/(?!\/)[^\n]*\n)+\s*\/\/\/\s*)?(?:^|\n)\s*(\w+)\s*(?:=\s*([^,\/]+)\s*)?,?\s*((?:\s*\/\/(?!\/)[^\n]*)*)(?=(?:\n|$))", re.S)
+
+def make_enum(cef_capi_name, cef_body):
+    result = ""
+
+    swift_type_name = cef_capi_type_to_swift(cef_capi_name)
+    result += "public enum " + swift_type_name + ": Int32 {\n"
+
+    head_comments = []
+    names = []
+    values = []
+    inline_comments = []
+    for value_match in re.finditer(enum_value_pattern, type_body):
+        head_comments.append(value_match.group(1))
+        names.append(value_match.group(2))
+        values.append(value_match.group(3))
+        inline_comments.append(value_match.group(4))
+
+    prefix = string.join(common_prefix(names).split("_")[:-1], "_")
+    for i in range(0, len(names)):
+        if head_comments[i] != None:
+            result += "\n" + indent_multiline_string(head_comments[i], 1) + "\n"
+        elif inline_comments[i] != None and len(inline_comments[i].strip()) > 0:
+            result += "\n" + indent_multiline_string("///\n" + inline_comments[i] + "\n///", 1) + "\n"
+            
+        swift_name = cef_capi_enum_entry_to_swift(names[i], prefix)
+        if names[i] in entries_render_as_property:
+            result += "    public static var " + swift_name + ": " + swift_type_name
+            result += " { get { return " + swift_type_name + "(rawValue: " + values[i].strip() + ")! } }\n" 
+        else:
+            result += "    case " + swift_name
+            if values[i] != None:
+                result += " = " + values[i].strip()
+            
+        result += "\n"
+
+    result += "}\n\n"
+    
+    cast = "UInt32"
+    if cef_capi_name in entrytype_tocef_cast_overrides:
+        cast = entrytype_tocef_cast_overrides[cef_capi_name]
+    
+    result += "extension " + swift_type_name + " {\n"
+    result += "    static func fromCEF(value: " + cef_capi_name + ") -> " + swift_type_name + " {\n"
+    result += "        return " + swift_type_name + "(rawValue: Int32(value.rawValue))!\n"
+    result += "    }\n\n"
+    result += "    func toCEF() -> " + cef_capi_name + " {\n"
+    result += "        return " + cef_capi_name + "(rawValue: " + cast + "(rawValue))\n"
+    result += "    }\n"
+    result += "}\n\n"
+
+    return result
+
+
+def make_option_set(cef_capi_name, cef_body):
+    result = ""
+
+    swift_type_name = cef_capi_type_to_swift(type_name)
+
+    result += "public struct " + swift_type_name + ": OptionSetType {\n"
+    result += "    public let rawValue: UInt32\n"
+    result += "    public init(rawValue: UInt32) {\n"
+    result += "        self.rawValue = rawValue\n"
+    result += "    }\n\n"
+
+    head_comments = []
+    names = []
+    values = []
+    inline_comments = []
+    for value_match in re.finditer(enum_value_pattern, type_body):
+        head_comments.append(value_match.group(1))
+        names.append(value_match.group(2))
+        values.append(value_match.group(3))
+        inline_comments.append(value_match.group(4))
+
+    prefix = string.join(common_prefix(names).split("_")[:-1], "_")
+    for i in range(0, len(names)):
+        if head_comments[i] != None:
+            result += "\n" + indent_multiline_string(head_comments[i], 1) + "\n"
+        elif inline_comments[i] != None and len(inline_comments[i].strip()) > 0:
+            result += "\n" + indent_multiline_string("///\n" + inline_comments[i] + "\n///", 1) + "\n"
+            
+        swift_name = cef_capi_enum_entry_to_swift(names[i], prefix)
+        swift_value = cef_capi_enum_value_to_swift(values[i].strip())
+        result += "    public static let " + swift_name + " = " + swift_type_name + "(rawValue: " + swift_value + ")\n"
+
+    result += "}\n\n"
+
+    result += "extension " + swift_type_name + " {\n"
+    result += "    static func fromCEF(value: " + cef_capi_name + ") -> " + swift_type_name + " {\n"
+    result += "        return " + swift_type_name + "(rawValue: UInt32(value.rawValue))\n"
+    result += "    }\n\n"
+    result += "    func toCEF() -> " + cef_capi_name + " {\n"
+    result += "        return " + cef_capi_name + "(rawValue: UInt32(rawValue))\n"
+    result += "    }\n"
+    result += "}\n\n"
+
+    return result
+
+
+def make_enum_header(swift_type_name, cef_header_name):
+    header = '''//
+//  %s.g.swift
+//  CEF.swift
+//
+//  This file was generated automatically from %s.
+//
+
+import Foundation
+
+'''
+    return header % (swift_type_name, cef_header_name)
+
+
+if len(sys.argv) < 3:
+    print "Usage: " + os.path.basename(sys.argv[0]) + " <cef_header> <dst_dir>"
+    exit(-1)
+    
+
+infile_path = sys.argv[1]
+dst_dir = sys.argv[2]
+
+infile = open(infile_path, "r")
+infile_contents = infile.read()
+infile.close()
+
+
+for type_match in re.finditer(enum_type_pattern, infile_contents):
+    type_comment = type_match.group(1)
+    type_body = type_match.group(2)
+    type_name = type_match.group(3)
+    
+    result = ""
+    result += indent_multiline_string(type_comment, 0)
+    
+    if type_name in cef_enums:
+        result += make_enum(type_name, type_body)
+    elif type_name in cef_option_sets:
+        result += make_option_set(type_name, type_body)
+    else:
+        if type_name in cef_hybrid_enums:
+            print "skipping hybrid type '" + type_name + "'"
+        else:
+            print "skipping unknown type '" + type_name + "'"
+        continue
+
+    swift_type_name = cef_capi_type_to_swift(type_name)
+    
+    outfile_contents = make_enum_header(swift_type_name, os.path.basename(infile_path))
+    outfile_contents += result
+    
+    if not os.access(dst_dir, os.F_OK):
+        os.makedirs(dst_dir)
+    
+    outfile = open(os.path.join(dst_dir, swift_type_name + ".g.swift"), "w")
+    outfile.write(outfile_contents)
+    outfile.close()
+
+
+# print find_enums(file_contents)
