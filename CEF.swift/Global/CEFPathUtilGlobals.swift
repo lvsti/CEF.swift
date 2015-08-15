27a066f104ca29ecbd09ad1af4939d4b19af6948
Tamas Lustyik <elveestei@gmail.com>
2015-08-15 10:07:30 +0200

    Path utils

---
+++ CEF.swift/Global/CEFPathUtilGlobals.swift
@@ -0,0 +1,20 @@
+//
+//  CEFPathUtilGlobals.swift
+//  CEF.swift
+//
+//  Created by Tamas Lustyik on 2015. 08. 14..
+//  Copyright © 2015. Tamas Lustyik. All rights reserved.
+//
+
+import Foundation
+
+///
+// Retrieve the path associated with the specified |key|. Returns true (1) on
+// success. Can be called on any thread in the browser process.
+///
+public func CEFGetPath(key: CEFPathKey) -> String? {
+    var cefStr = cef_string_t()
+    let retval = cef_get_path(key.toCEF(), &cefStr)
+    return retval != 0 ? CEFStringToSwiftString(cefStr) : nil
+}
+
