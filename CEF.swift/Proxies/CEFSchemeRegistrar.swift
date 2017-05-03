55b7850ae8751f445a503347e36db548da6784dc
Tamas Lustyik <elveestei@gmail.com>
2017-05-03 20:29:04 +0200

    Updated proxies

--- CEF.swift/Proxies/CEFSchemeRegistrar.swift
+++ CEF.swift/Proxies/CEFSchemeRegistrar.swift
@@ -57,8 +57,12 @@ public extension CEFSchemeRegistrar {
     /// rules as those applied to "https" URLs. For example, loading this scheme
     /// from other secure schemes will not trigger mixed content warnings.
     ///
-    /// If |is_cors_enabled| is true the scheme that can be sent CORS requests.
-    /// This value should be true in most cases where |is_standard| is true.
+    /// If |is_cors_enabled| is true the scheme can be sent CORS requests. This
+    /// value should be true in most cases where |is_standard| is true.
+    ///
+    /// If |is_csp_bypassing| is true the scheme can bypass Content-Security-Policy
+    /// (CSP) checks. This value should be false in most cases where |is_standard|
+    /// is true.
     ///
     /// This function may be called on any thread. It should only be called once
     /// per unique |scheme_name| value. If |scheme_name| is already registered or
@@ -70,7 +74,8 @@ public extension CEFSchemeRegistrar {
                                 isLocal: Bool,
                                 isDisplayIsolated: Bool,
                                 isSecure: Bool,
-                                isCORSEnabled: Bool) -> Bool {
+                                isCORSEnabled: Bool,
+                                isCSPBypassing: Bool) -> Bool {
         let cefStrPtr = CEFStringPtrCreateFromSwiftString(name)
         defer { CEFStringPtrRelease(cefStrPtr) }
         return cefObject.add_custom_scheme(cefObjectPtr,
@@ -79,7 +84,8 @@ public extension CEFSchemeRegistrar {
                                            isLocal ? 1 : 0,
                                            isDisplayIsolated ? 1 : 0,
                                            isSecure ? 1 : 0,
-                                           isCORSEnabled ? 1 : 0) != 0
+                                           isCORSEnabled ? 1 : 0,
+                                           isCSPBypassing ? 1 : 0) != 0
     }
     
 }
