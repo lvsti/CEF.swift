b60cf51baf23e05c6e2e19d844a3e6bc22a72640
Tamas Lustyik <elveestei@gmail.com>
2017-10-04 21:26:47 +0200

    Pimped transcription checker scripts

--- scripts/check_proxy_impl.py
+++ scripts/check_proxy_impl.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from __future__ import print_function
 import sys
 import os
 import string
@@ -9,6 +10,7 @@ from cef_parser import *
 
 cefnames_pattern = re.compile("\/\/\/ CEF name: (`[^`]+`(?:\s*,\s*`[^`]+`)*)")
 
+missing_implementations = []
 
 def check_proxy_impl(cef_class, impl):
     interface_funcs = cef_class.get_static_funcs() + cef_class.get_virtual_funcs()
@@ -21,11 +23,13 @@ def check_proxy_impl(cef_class, impl):
     
     for func in interface_funcs:
         if not func.get_name() in referenced_funcs:
-            print func.get_name() + " not implemented in " + swift_type_name
+            global missing_implementations
+            missing_implementations.append(swift_type_name + '.' + func.get_name())
+            print(func.get_name() + " not implemented in " + swift_type_name, file=sys.stderr)
 
 
 if len(sys.argv) < 3:
-    print "Usage: " + os.path.basename(sys.argv[0]) + " <cef_includes_dir> <proxy_impl_dir>"
+    print("Usage: " + os.path.basename(sys.argv[0]) + " <cef_includes_dir> <proxy_impl_dir>")
     exit(-1)
 
 includes_dir = sys.argv[1]
@@ -43,7 +47,8 @@ for cef_class in header.get_classes():
         
         impl_file_path = os.path.join(impl_dir, swift_type_name + ".swift")
         if not os.path.exists(impl_file_path):
-            print "implementation missing for " + swift_type_name
+            missing_implementations.append(swift_type_name)
+            print("implementation missing for " + swift_type_name, file=sys.stderr)
             continue
             
         impl_file = open(impl_file_path, "r")
@@ -51,3 +56,6 @@ for cef_class in header.get_classes():
         impl_file.close()
         
         check_proxy_impl(cef_class, impl)
+
+if len(missing_implementations) > 0:
+    exit(-2)
