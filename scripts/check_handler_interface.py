bd3e905061535d94536999611a75be8d96ef6d75
Tamas Lustyik <elveestei@gmail.com>
2017-10-04 21:26:47 +0200

    Pimped transcription checker scripts

--- scripts/check_handler_interface.py
+++ scripts/check_handler_interface.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from __future__ import print_function
 import sys
 import os
 import string
@@ -9,6 +10,7 @@ from cef_parser import *
 
 cefnames_pattern = re.compile("\/\/\/ CEF name: (`[^`]+`(?:\s*,\s*`[^`]+`)*)")
 
+missing_declarations = []
 
 def check_handler_interface(cef_class, interface):
     interface_funcs = cef_class.get_virtual_funcs()
@@ -21,11 +23,13 @@ def check_handler_interface(cef_class, interface):
     
     for func in interface_funcs:
         if not func.get_name() in referenced_funcs:
-            print func.get_name() + " not declared in " + swift_type_name
+            global missing_declarations
+            missing_declarations.append(swift_type_name + '.' + func.get_name())
+            print(func.get_name() + " not declared in " + swift_type_name, file=sys.stderr)
 
 
 if len(sys.argv) < 3:
-    print "Usage: " + os.path.basename(sys.argv[0]) + " <cef_includes_dir> <handler_interface_dir>"
+    print("Usage: " + os.path.basename(sys.argv[0]) + " <cef_includes_dir> <handler_interface_dir>")
     exit(-1)
 
 includes_dir = sys.argv[1]
@@ -41,8 +45,17 @@ for cef_class in header.get_classes():
     if not cef_class.is_library_side():
         swift_type_name = cef_capi_type_to_swift(cef_class.get_capi_name())
         
-        interface_file = open(os.path.join(interface_dir, swift_type_name + ".swift"), "r")
+        if_file_path = os.path.join(interface_dir, swift_type_name + ".swift")
+        if not os.path.exists(if_file_path):
+            missing_implementations.append(swift_type_name)
+            print("interface missing for " + swift_type_name, file=sys.stderr)
+            continue
+        
+        interface_file = open(if_file_path, "r")
         interface = interface_file.read()
         interface_file.close()
         
         check_handler_interface(cef_class, interface)
+
+if len(missing_declarations) > 0:
+    exit(-2)
