ab2d84c970e4f7f762d2d90f2ff8af251d06ac49
Tamas Lustyik <elveestei@gmail.com>
2018-02-10 19:41:39 +0100

    Helper functions for string conversion

--- CEF.swift/Internal/CEFString.swift
+++ CEF.swift/Internal/CEFString.swift
@@ -15,7 +15,21 @@ func CEFStringToSwiftString(_ cefStr: cef_string_t) -> String {
     return String(NSString(characters: cefStr.str, length: cefStr.length))
 }
 
-func CEFStringPtrCreateFromSwiftString(_ str: String) -> cef_string_userfree_utf16_t! {
+func CEFStringPtrToSwiftString(_ ptr: UnsafePointer<cef_string_t>?, defaultValue: String) -> String {
+    guard let str = ptr?.pointee else {
+        return defaultValue
+    }
+    return CEFStringToSwiftString(str)
+}
+
+func CEFStringPtrToSwiftString(_ ptr: UnsafePointer<cef_string_t>?) -> String? {
+    guard let cefStr = ptr?.pointee else {
+        return nil
+    }
+    return CEFStringToSwiftString(cefStr)
+}
+
+func CEFStringPtrCreateFromSwiftString(_ str: String) -> cef_string_userfree_utf16_t {
     let cefStr = cef_string_userfree_utf16_alloc()!
     CEFStringSetFromSwiftString(str, cefStringPtr: cefStr)
     return cefStr
