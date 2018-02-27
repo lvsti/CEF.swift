b0b0ff34d05f858c2151e6195244ca2b3b897496
Tamas Lustyik <elveestei@gmail.com>
2018-02-27 22:34:09 +0100

    Enhanced Swift helper app demo to show the basics of renderer implementation

--- Samples/CEFDemo/CEFDemo Helper/main.swift
+++ Samples/CEFDemo/CEFDemo Helper/main.swift
@@ -9,6 +9,33 @@
 import Darwin
 import CEFswift
 
+class App: CEFApp, CEFRenderProcessHandler {
+    var renderProcessHandler: CEFRenderProcessHandler? {
+        return self
+    }
+    
+    func onContextCreated(browser: CEFBrowser, frame: CEFFrame, context: CEFV8Context) {
+        context.enter()
+        let v8Str = CEFV8Value.createString("Hello World!")
+        print("\(v8Str!.stringValue)")
+        context.exit()
+    }
+}
+
 let mainArgs = CEFMainArgs(arguments: CommandLine.arguments)
-let retval = CEFProcessUtils.executeProcess(with: mainArgs)
+let commandLine = CEFCommandLine()!
+commandLine.initFromArguments(CommandLine.arguments)
+
+let retval: Int
+if commandLine.switchValue(for: "type") == "renderer" {
+    // renderer process
+    // uncomment the following line in order to have the process paused until a debugger is attached
+    //raise(SIGSTOP)
+    retval = CEFProcessUtils.executeProcess(with: mainArgs, app: App())
+}
+else {
+    // other helper processes spawned by CEF
+    retval = CEFProcessUtils.executeProcess(with: mainArgs)
+}
+
 exit(Int32(retval))
