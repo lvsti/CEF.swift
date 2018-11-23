af3f6e9b3a942b716c8a030a0b04ad11c2e2fd9d
Tamas Lustyik <elveestei@gmail.com>
2018-11-24 00:13:48 +0100

    Implemented sandbox in C/C++ helpers

--- Samples/CEFDemo/CEFDemo Helper/main.c
+++ Samples/CEFDemo/CEFDemo Helper/main.c
@@ -7,10 +7,38 @@
 //
 
 #include "include/capi/cef_app_capi.h"
+#include "include/wrapper/cef_library_loader.h"
+#include "include/cef_sandbox_mac.h"
+#include <libgen.h>
+#include <stdio.h>
 
+// Entry point function for sub-processes.
 int main(int argc, char* argv[]) {
-    cef_main_args_t mainArgs = {.argc = argc, .argv = argv };
+    // Initialize the macOS sandbox for this helper process.
+    void* sandboxContext = cef_sandbox_initialize(argc, argv);
+    if (!sandboxContext) {
+        return 1;
+    }
     
-    return cef_execute_process(&mainArgs, NULL, NULL);
+    // Load the CEF framework library at runtime instead of linking directly
+    // as required by the macOS sandbox implementation.
+    char fwPath[1024];
+    snprintf(fwPath, 1024, "%s/../../../Chromium Embedded Framework.framework/Chromium Embedded Framework", dirname(argv[0]));
+    
+    if (!cef_load_library(fwPath)) {
+        cef_sandbox_destroy(sandboxContext);
+        return 1;
+    }
+    
+    // Provide CEF with command-line arguments.
+    cef_main_args_t mainArgs = {.argc = argc, .argv = argv};
+    
+    // Execute the sub-process.
+    int retval = cef_execute_process(&mainArgs, NULL, NULL);
+    
+    cef_unload_library();
+    cef_sandbox_destroy(sandboxContext);
+    
+    return retval;
 }
 
--- Samples/CEFDemo/CEFDemo Helper/main.cpp
+++ Samples/CEFDemo/CEFDemo Helper/main.cpp
@@ -13,15 +13,17 @@
 // Entry point function for sub-processes.
 int main(int argc, char* argv[]) {
     // Initialize the macOS sandbox for this helper process.
-    CefScopedSandboxContext sandbox_context;
-    if (!sandbox_context.Initialize(argc, argv))
+    CefScopedSandboxContext sandboxContext;
+    if (!sandboxContext.Initialize(argc, argv)) {
         return 1;
+    }
     
     // Load the CEF framework library at runtime instead of linking directly
     // as required by the macOS sandbox implementation.
-    CefScopedLibraryLoader library_loader;
-    if (!library_loader.LoadInHelper())
+    CefScopedLibraryLoader libraryLoader;
+    if (!libraryLoader.LoadInHelper()) {
         return 1;
+    }
     
     // Provide CEF with command-line arguments.
     cef_main_args_t mainArgs = {.argc = argc, .argv = argv};
