a1a5426dc0345d7f2c0dcebab77977a2e49de4af
Tamas Lustyik <elveestei@gmail.com>
2020-04-29 08:21:27 +0200

    Added missing file to please the CI

---
+++ CEF.swift/Proxies/CEFRegistration.swift
@@ -0,0 +1,13 @@
+//
+//  CEFRegistration.swift
+//  CEF.swift
+//
+//  Created by Tamas Lustyik on 2020. 04. 29..
+//  Copyright © 2020. Tamas Lustyik. All rights reserved.
+//
+
+import Foundation
+
+public extension CEFRegistration {
+    
+}
