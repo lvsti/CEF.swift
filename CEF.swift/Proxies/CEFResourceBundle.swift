6ec98570bf7085b4f2c138a927de0ad23e06e822
Tamas Lustyik <elveestei@gmail.com>
2020-10-31 20:45:24 +0100

    Updated proxies

--- CEF.swift/Proxies/CEFResourceBundle.swift
+++ CEF.swift/Proxies/CEFResourceBundle.swift
@@ -27,38 +27,26 @@ extension CEFResourceBundle {
         return CEFStringPtrToSwiftString(cefStrPtr)
     }
     
-    /// Retrieves the contents of the specified scale independent |resource_id|.
-    /// If the value is found then |data| and |data_size| will be populated and
-    /// this method will return true. If the value is not found then this method
-    /// will return false. The returned |data| pointer will remain resident in
-    /// memory and should not be freed. Include cef_pack_resources.h for a listing
-    /// of valid resource ID values.
+    /// Returns a CefBinaryValue containing the decompressed contents of the
+    /// specified scale independent |resource_id| or NULL if not found. Include
+    /// cef_pack_resources.h for a listing of valid resource ID values.
     /// CEF name: `GetDataResource`
-    public func dataResource(for resourceID: Int) -> (dataBufferPtr: UnsafeMutableRawPointer?, dataSize: size_t)? {
-        var dataPtr: UnsafeMutableRawPointer? = nil
-        var size: size_t = 0
-        let result = cefObject.get_data_resource(cefObjectPtr, Int32(resourceID), &dataPtr, &size)
-        return result != 0 ? (dataPtr, size) : nil
+    public func dataResource(for resourceID: Int) -> CEFBinaryValue? {
+        let cefData = cefObject.get_data_resource(cefObjectPtr, Int32(resourceID))
+        return CEFBinaryValue.fromCEF(cefData)
     }
-    
-    /// Retrieves the contents of the specified |resource_id| nearest the scale
-    /// factor |scale_factor|. Use a |scale_factor| value of SCALE_FACTOR_NONE for
-    /// scale independent resources or call GetDataResource instead. If the value
-    /// is found then |data| and |data_size| will be populated and this method will
-    /// return true. If the value is not found then this method will return false.
-    /// The returned |data| pointer will remain resident in memory and should not
-    /// be freed. Include cef_pack_resources.h for a listing of valid resource ID
-    /// values.
+
+    // Returns a CefBinaryValue containing the decompressed contents of the
+    // specified |resource_id| nearest the scale factor |scale_factor| or NULL if
+    // not found. Use a |scale_factor| value of SCALE_FACTOR_NONE for scale
+    // independent resources or call GetDataResource instead.Include
+    // cef_pack_resources.h for a listing of valid resource ID values.
     /// CEF name: `GetDataResourceForScale`
-    public func dataResource(for resourceID: Int, scale: CEFScaleFactor) -> (dataBufferPtr: UnsafeMutableRawPointer?, dataSize: size_t)? {
-        var dataPtr: UnsafeMutableRawPointer? = nil
-        var size: size_t = 0
-        let result = cefObject.get_data_resource_for_scale(cefObjectPtr,
+    public func dataResource(for resourceID: Int, scale: CEFScaleFactor) -> CEFBinaryValue? {
+        let cefData = cefObject.get_data_resource_for_scale(cefObjectPtr,
                                                            Int32(resourceID),
-                                                           scale.toCEF(),
-                                                           &dataPtr,
-                                                           &size)
-        return result != 0 ? (dataPtr, size) : nil
+                                                           scale.toCEF())
+        return CEFBinaryValue.fromCEF(cefData)
     }
 
 }
